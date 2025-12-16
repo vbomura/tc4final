0d4e0652ebb192c97440a1e1ae4d199d8f56bd2e
Vitor Bomura <vitorbomura@gmail.com>
2025-12-16 20:45:11 -0300

    Fix typo in Decision Tree description

--- modules/imagem.py
+++ modules/imagem.py
@@ -9,7 +9,7 @@ def app():
 
         Para fazer a predição identificamos seis variáveis que tem correlação forte com a categoria de peso (peso, altura, idade, histórico familiar de obesidade, consumo entre refeições e consumo frequente de calorias). Para testar os modelos com as variáveis criamos um pipeline, nele pudemos parametrizar e validar cada um deles. 
                 
-        **Com 94%% de precisão, Decision Tree** foi o melhor modelo, seguido de Random Forest (mesmo resultado de 94%) e Regressão Logística (52%). O resultado igual de Random Forest e Decision Tree é condizente considerando que o primeiro se trata de fazer o segundo várias vezes. Dito isso, para uso menor de recursos optamos pelo Decision Tree.
+        **Com 94% de precisão, Decision Tree** foi o melhor modelo, seguido de Random Forest (mesmo resultado de 94%) e Regressão Logística (52%). O resultado igual de Random Forest e Decision Tree é condizente considerando que o primeiro se trata de fazer o segundo várias vezes. Dito isso, para uso menor de recursos optamos pelo Decision Tree.
 
         Entrando em mais detalhes sobre o resultado, Decion Tree apresentou uma precisão muito boa em todos as faixas de peso, se mantendo acima 90% em todas as faixas. Sua taxa de recall, ou seja, falsos positivos, também foi muito boa, mostrando que o algoritmo consegue classificar com eficácia cada categoria.
         
